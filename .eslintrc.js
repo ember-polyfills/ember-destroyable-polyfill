d8e6274e109e248901f0057b53cdd11033743a81
Robert Jackson <rjackson@linkedin.com>
2020-07-31 18:49:51 -0400

    Refactor linting setup towards default ember addon setup.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,71 @@
 
 module.exports = {
   root: true,
-  extends: '@clark/node'
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'prettier'],
+  env: {
+    node: true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+
+  overrides: [
+    // node files
+    {
+      files: [
+        '.eslintrc.js',
+        '.prettierrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'index.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js',
+      ],
+      excludedFiles: [
+        'addon/**',
+        'addon-test-support/**',
+        'app/**',
+        'tests/dummy/app/**',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+    },
+
+    // TypeScript files
+    {
+      files: ['**/*.ts'],
+
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+      },
+
+      plugins: ['@typescript-eslint'],
+      extends: ['plugin:@typescript-eslint/eslint-recommended'],
+      rules: {
+        'no-array-constructor': 'off',
+        '@typescript-eslint/no-array-constructor': 'error',
+        'no-empty-function': 'off',
+        '@typescript-eslint/no-empty-function': 'error',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+      },
+    },
+  ],
 };
